fff580ba5428d444c5fb5fb550940d31db029dd4
pallavi002 <pallavilodhi08@gmail.com>
2021-01-16 00:14:31 +0530

    wague two way binding

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import career from './Career/Career';
 import Career from './Career/Career'
 
 class App extends Component {
@@ -25,12 +26,21 @@ class App extends Component {
       luck: "Hey good luck !!"
     })
   }
+  careerChangedHandler = (event) => {
+    this.setState({
+      careers: [
+        {id:"key01", course: event.target.value},
+        {id:"key02", course:"Human Resources"},
+        {id:"key03", course:"Designer"}
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi..This is the life of pallavi.</h1>
         <button onClick={ () => this.switchCareerHandler("army")}>SWITCH CAREER</button>
-        <Career course={this.state.careers[0].course} />
+        <Career course={this.state.careers[0].course} careerChanged={this.careerChangedHandler} />
         <Career course={this.state.careers[1].course} clicked={this.switchCareerHandler.bind(this, "Just sleeping")} />
         <Career course={this.state.careers[2].course} clickGoodLuck={this.printGoodLuck} luck={this.state.luck}>Go for MBA</Career>
       </div>
--- src/Career/Career.js
+++ src/Career/Career.js
@@ -6,6 +6,7 @@ const career = (props) => {
             <p onClick={props.clicked}>Start working on your career with {props.course} before it's too late</p>
             <p>{props.children}</p>
             <p onClick={props.clickGoodLuck}>{props.luck}</p>
+            Suggest Career: <input type="text" onChange={props.careerChanged}></input>
         </div>
     )
 }
