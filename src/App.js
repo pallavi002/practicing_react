7addf81f84dcbfa8270d4dd804843dd160c8b0d1
pallavi002 <pallavilodhi08@gmail.com>
2021-01-15 22:33:46 +0530

    norm vs hook

--- src/App.js
+++ src/App.js
@@ -1,3 +1,39 @@
+import React, { useState } from 'react';  //useState is a react hook
+import './App.css';
+import Career from './Career/Career'
+
+const App = (props) => {
+  const [careerState, newCareerState] = useState({
+      careers: [
+        { id: "key01", course: "coding" },
+        { id: "key02", course: "finance"},
+        { id: "key03", course: "Dance" }
+      ]
+  });
+  const switchCareerHandler = () => {
+    newCareerState({
+      careers: [
+        { id: "key01", course: "Cyber Security" },
+        { id: "key02", course: "Human Resources" },
+        { id: "key03", course: "Designer" }
+      ]
+    })
+  };
+
+  return (
+    <div className="App">
+      <h1>Hi..This is the life of pallavi.</h1>
+      <button onClick={switchCareerHandler}>SWITCH CAREER</button>
+      <Career course={careerState.careers[0].course} />
+      <Career course={careerState.careers[1].course} />
+      <Career course={careerState.careers[2].course} >Go for MBA</Career>
+    </div>
+  );
+}
+
+export default App;
+
+/*
 import React, {Component} from 'react';
 import './App.css';
 import Career from './Career/Career'
@@ -28,8 +64,9 @@ class App extends Component {
         <Career course={this.state.careers[1].course} />
         <Career course={this.state.careers[2].course} >Go for MBA</Career>
       </div>
-    ); 
+    );
   }
 }
 
 export default App;
+*/
